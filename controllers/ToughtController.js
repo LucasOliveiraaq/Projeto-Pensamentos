76363823fdfc612fb0321cc1a02df3a106836cdd
Lucas P Oliveira <lucasp.oliveira122@gmail.com>
2023-11-13 06:48:45 -0300

    Funcionalidade busca e ordenação

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,14 +1,39 @@
 const Tought = require('../models/Tought');
 const User = require('../models/User');
 const messages = require('../models/Messages');
+const { Op } = require('sequelize');
 
 module.exports = class ToughtsController {
     static async showToughts(req, res) {
+        let search = '';
+
+        if(req.query.search) {
+            search = req.query.search;
+        }
+
+        let order = 'DESC';
+
+        if(req.query.order === 'old'){
+            order = 'ASC';
+        } else {
+            order = 'DESC';
+        }
+
         const toughtsData = await Tought.findAll({
             include: User,
+            where: {
+                title: {[Op.like]: `%${search}%`},
+            },
+            order: [['createdAt', order]],
         });
         const toughts = toughtsData.map((result) => result.get({plain: true}));
-        res.render('toughts/home', {toughts});
+
+        let contadorToughts = toughts.length;
+        if(contadorToughts === 0){
+            contadorToughts = false;
+        }
+
+        res.render('toughts/home', {toughts, search, contadorToughts});
     }
 
     static async dashboard(req, res) {
